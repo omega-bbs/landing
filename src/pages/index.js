24a3f1ff19bdec0610d1f98b3278770b9bde2c3b
IntPtr <lxy.lixiaoyan@gmail.com>
2017-09-30 16:24:02 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+import "./index.css";
+
+const Index = () => (
+  <div>
+    <Helmet title="ω bbs" />
+    <main className="page">
+      <header className="header">
+        <h1 className="title">ω bbs</h1>
+        <h2 className="slogan">Next-generation Forum Platform</h2>
+      </header>
+      <footer className="footer">
+        <p className="deadline">2017. 12. 31</p>
+        <p className="coming-soon">Coming Soon</p>
+      </footer>
+    </main>
+  </div>
+);
+
+export default Index;
